8d81d532e12ec3d50896f6bd7706d020546caf41
22051055 <n.m220510@gmail.com>
2025-07-29 15:42:41 +0900

    Add files via upload

---
+++ src/test/java/com/example/image_watermark_app_clean/ImageWatermarkAppCleanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.image_watermark_app_clean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageWatermarkAppCleanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
