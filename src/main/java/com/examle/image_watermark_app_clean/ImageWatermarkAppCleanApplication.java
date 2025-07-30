e09d65f88320c114a7f30c85b014c16cf8916701
22051055 <n.m220510@gmail.com>
2025-07-30 01:36:44 +0000

    FIX

---
+++ src/main/java/com/examle/image_watermark_app_clean/ImageWatermarkAppCleanApplication.java
@@ -0,0 +1,13 @@
+package com.example.image_watermark_app_clean; // 正しいパッケージ名であることを確認
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // Spring Bootアプリケーションの開始点を示すアノテーション
+public class ImageWatermarkAppCleanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageWatermarkAppCleanApplication.class, args);
+    }
+
+}
